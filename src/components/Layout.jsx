c23137fd714c1a17043c4b79f2f72247d90d46e8
Jhordan Reyes <reyesjhordan4@gmail.com>
2022-12-31 16:36:43 -0500

    Creación del componente layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,9 +3,12 @@ import { Outlet } from "react-router-dom"
 
 const Layout = () => {
     return (
-        <div>
-            <h1 className='text-6xl font-bold'>Jhordan</h1>
-            <Outlet></Outlet>
+        <div className="md:flex md:min-h-screen">
+            <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
+                <h2>CRM - clientes</h2>
+            </aside>
+            <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
+            </main>
         </div>
     )
 }
